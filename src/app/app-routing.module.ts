38458de934af52c8fd0859a5dc637deaeb86a6b9
Valerie1801 <69993850+Valerie1801@users.noreply.github.com>
2020-10-10 01:14:35 -0500

    explotacion de todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactenosComponent } from "./paginas/contactenos/contactenos.component"
+import { CorosComponent } from "./paginas/coros/coros.component"
+import { CremacionesComponent } from "./paginas/cremaciones/cremaciones.component"
+import { FloresComponent } from "./paginas/flores/flores.component"
+import { HomeComponent } from "./paginas/home/home.component"
+import { SepeliosComponent } from "./paginas/sepelios/sepelios.component"
 
-const routes: Routes = [];
+import { ServiciosComponent } from "./paginas/servicios/servicios.component"
+import { TarifasComponent } from "./paginas/tarifas/tarifas.component"
+
+const routes: Routes = [ { path: 'contactenos', component: ContactenosComponent },
+{ path: 'coros', component: CorosComponent },
+{ path: 'cremaciones', component: CremacionesComponent },
+{ path: 'flores', component: FloresComponent },
+{ path: '', component: HomeComponent },
+{ path: 'sepelios', component: SepeliosComponent },
+{ path: 'servicios', component: ServiciosComponent },
+{ path: 'tarifas', component: TarifasComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/componentes/cards/cards.component.ts
+++ src/app/componentes/cards/cards.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  @Input() rutaImagen:string;
+  @Input() title:string;
+
   constructor() { }
 
   ngOnInit(): void {
